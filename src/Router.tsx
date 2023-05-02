e7a7c0c794a670dbd20423246d718630e7835c63
eunyechan <glqgkq384@naver.com>
2023-05-02 20:06:14 +0900

    feat: 폴더 세팅

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "../src/components/common/Header";
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route />
       </Routes>
---
+++ src/components/apis/api.ts
@@ -0,0 +1,5 @@
+// 타입지정 예시
+//  export type ISignup = {
+//      name: string;
+//      email: string;
+// }
