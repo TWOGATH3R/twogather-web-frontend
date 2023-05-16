730ef72d5f27a3abef8b4939299f520db658f84e
taewok <taewok5163@naver.com>
2023-05-16 12:42:51 +0900

    #17 feat: code 유효 시간 timer 구현

--- src/components/RegisterPage/EmailConfirm.tsx
+++ src/components/RegisterPage/EmailConfirm.tsx
@@ -8,30 +8,69 @@ const EmailConfirm = () => {
 
   const [email, setEmail] = useState<string>("");
   const [code, setCode] = useState<string>("");
+  const [timer, setTimer] = useState<string>("");
+  const [codeConfirm, setCodeConfrim] = useState<boolean>(false);
 
+  //onChange
   const emailPattern =
     /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
   const emailOnChange = (emailText: string) => {
     setEmail(emailText);
+  };
+  const codeOnChange = (codeText: string) => {
+    setCode(codeText);
   };
 
   //onClick
   const emailBtnOnClick = () => {
     if (email === "") alert("이메일을 입력해주세요");
     else if (!emailPattern.test(email)) alert("이메일이 형식에 맞지 않습니다");
+    else timerInterval();
   };
   const codeBtnOnClick = () => {
     if (code === "") alert("인증번호를 입력해주세요");
     else if (!code === null) alert("인증번호가 알맞지 않습니다");
+    else {
+      alert("인증완료");
+      setCodeConfrim(true);
+    }
   };
   const nextBtnOnClick = () => {
-    navigate(`/register/${Param.RegisterType}/Privacy`);
+    if (email === "") alert("이메일을 입력해주세요");
+    else if (!emailPattern.test(email)) alert("이메일 형식을 맞춰주세요");
+    else if (code === "") alert("인증번호를 입력해주세요");
+    else if (!codeConfirm) alert("인증번호 확인을 완료해주세요");
+    else
+      navigate(`/register/${Param.RegisterType}/Privacy`, {
+        state: {
+          email: email,
+        },
+      });
+  };
+
+  //인증코드 유효 타이머
+  const timerInterval = () => {
+    var time: any = 600;
+    var min: any = 0;
+    var sec: any = 0;
+
+    var x = setInterval(() => {
+      min = time / 60;
+      sec = time % 60;
+      setTimer(`${Math.floor(min)}분${sec}초`);
+      time--;
+      if (time < 0) {
+        clearInterval(x);
+        setTimer(`시간초과`);
+      }
+    }, 1000);
   };
 
   return (
     <>
       <EmailBox valid={email.length > 0 ? emailPattern.test(email) : true}>
         <EmailInput
+          value={email}
           placeholder="이메일"
           onChange={(e) => emailOnChange(e.target.value)}
         />
@@ -40,8 +79,13 @@ const EmailConfirm = () => {
         </EmailSendBtn>
       </EmailBox>
       <ConfirmBox valid={true}>
-        <ConfirmInput placeholder="인증코드" />
+        <ConfirmInput
+          value={code}
+          placeholder="인증코드"
+          onChange={(e) => codeOnChange(e.target.value)}
+        />
         <ConfirmBtn onClick={() => codeBtnOnClick()}>인증</ConfirmBtn>
+        {timer && <TimerBox>{timer}</TimerBox>}
       </ConfirmBox>
       <NextBtn onClick={() => nextBtnOnClick()}>다음</NextBtn>
     </>
@@ -75,6 +119,7 @@ const EmailBox = styled.div<{ valid: any }>`
 const EmailInput = styled.input`
   margin-right: 15px;
   width: 50%;
+  border: 1px solid rgba(0, 0, 0, 0.2);
 `;
 const EmailSendBtn = styled.button`
   padding: 5px 10px;
@@ -86,7 +131,20 @@ const EmailSendBtn = styled.button`
   color: ${({ theme }) => theme.colors.white};
 `;
 
-const ConfirmBox = styled(EmailBox)``;
+const ConfirmBox = styled(EmailBox)`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 45px;
+  width: 100%;
+`;
+const TimerBox = styled.div`
+  position: absolute;
+  top: 100%;
+  left: calc(10% + 10px);
+  font-size: ${({ theme }) => theme.fontSizes.small};
+  color: #ff0000;
+`;
 const ConfirmInput = styled(EmailInput)``;
 const ConfirmBtn = styled(EmailSendBtn)``;
 
