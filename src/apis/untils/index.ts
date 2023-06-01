658f0f2f04246b0d6ecab85242e9e148e59dcefe
taewok <taewok5163@naver.com>
2023-06-01 19:44:52 +0900

    fix: 배포 상태에서 백엔드 api 통신 설정

--- src/apis/untils/index.ts
+++ src/apis/untils/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const host = window.location.hostname === "localhost" 
   ? 'http://ec2-15-165-96-247.ap-northeast-2.compute.amazonaws.com:8080'
-  : "api";
+  : "";
 
 export const api = axios.create({
   baseURL: host,
