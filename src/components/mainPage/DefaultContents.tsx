9f8ebd1383c3cd74dedfd9cdab1152fd656dc540
taewok <taewok5163@naver.com>
2023-06-04 18:58:20 +0900

    #2 feat: 기본 컨텐츠와 검색 했을떄의 컨텐츠 차별을 위해 라우팅 설정

---
+++ src/components/mainPage/DefaultContents.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReviewTop10 from "./ReviewTop10";
+import GradeTop10 from "./GradeTop10";
+import LikeTop10 from "./LikeTop10";
+
+
+const DefaultContents = () => {
+  return (
+    <>
+      <ReviewTop10 />
+      <GradeTop10 />
+      <LikeTop10 />
+    </>
+  );
+};
+
+export default DefaultContents;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import Search from "../components/mainPage/Search";
-import ReviewTop10 from "../components/mainPage/ReviewTop10";
-import GradeTop10 from "../components/mainPage/GradeTop10";
-import LikeTop10 from "../components/mainPage/LikeTop10";
+import { Outlet } from "react-router-dom";
 
 const Main = () => {
   return (
     <MainContainer>
       <Search />
-      <ReviewTop10 />
-      <GradeTop10 />
-      <LikeTop10 />
+      <Outlet/>
     </MainContainer>
   );
 };
