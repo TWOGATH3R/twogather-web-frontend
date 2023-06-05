f56e15883c583c95ec754bded46b24a482002173
taewok <taewok5163@naver.com>
2023-06-05 20:09:25 +0900

    #47 feat: 마이페이지 고객 정보변경 api

---
+++ src/apis/queries/MyPageQuery.ts
@@ -0,0 +1,32 @@
+import { getCookie } from "../../components/cookie/cookie";
+import { api } from "../untils";
+
+type infoType = {
+  email: string;
+  username: string;
+  password: string;
+  name: string;
+  memberId: string | null;
+};
+//고객 정보 업데이트하기
+export const putInfoChange = async (info: infoType) => {
+  console.log(info.memberId);
+  const res = await api.put(
+    `/api/consumers/${info.memberId}`,
+    {
+      email: info.email,
+      username: info.username,
+      password: info.password,
+      name: info.name,
+    },
+    {
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        accept: "application/json,",
+        Authorization: `Bearer ${getCookie("accessToken")}`,
+      },
+    }
+  );
+
+  return res.data;
+};
