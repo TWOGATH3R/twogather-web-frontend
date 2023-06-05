7edc9034c270f7aff1f49141a3b934383f0cd089
taewok <taewok5163@naver.com>
2023-06-05 20:09:58 +0900

    #47 feat: 마이페이지 고객 정보 query 생성 & test 완료

--- src/components/myPage/Info.tsx
+++ src/components/myPage/Info.tsx
@@ -1,30 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import { useMutation } from "react-query";
 import styled, { css } from "styled-components";
+import { putInfoChange } from "../../apis/queries/MyPageQuery";
 
 const Info = () => {
+  const [id, setId] = useState<string>("");
+  const [pw, setPw] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [name, setName] = useState<string>("");
+
+  const info = {
+    email: email,
+    username: id,
+    password: pw,
+    name: name,
+    memberId: localStorage.getItem("memberId")
+  };
+  const { mutate: infoChange } = useMutation(() => putInfoChange(info), {
+    onSuccess: (res) => {
+      alert("수정 성공")
+    },
+    onError: (err: any) => {
+      alert(err.response.data.message);
+    },
+  });
+
+  //onChange
+  const idPattern = /^(?=.*[a-zA-Z])(?=.*[0-9]).{4,15}$/;
+  const idOnChange = (idText: string) => {
+    setId(idText);
+  };
+  const pwPattern = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,20}$/;
+  const pwOnChange = (pwText: string) => {
+    setPw(pwText);
+  };
+  const emailPattern =
+    /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+  const emailOnChange = (emailText: string) => {
+    setEmail(emailText);
+  };
+  const nameOnChange = (nameText: string) => {
+    setName(nameText);
+  };
+
+  //onClick
+  const saveBtnOnClick = () => {
+    if (localStorage.getItem("role") === "ROLE_CONSUMER") infoChange();
+    else console.log("사업자 전용 함수자리");
+  };
+
   return (
     <SignUpContainer>
-      <IdBox valid={false}>
+      <IdBox valid={id.length > 0 ? idPattern.test(id) : true}>
         <IdText>아이디</IdText>
-        <IdInput />
+        <IdInput value={id} onChange={(e) => idOnChange(e.target.value)} />
       </IdBox>
-      <EmailBox valid={true}>
+      <EmailBox valid={email.length > 0 ? emailPattern.test(email) : true}>
         <EmailText>이메일</EmailText>
-        <EmailInput />
+        <EmailInput
+          value={email}
+          onChange={(e) => emailOnChange(e.target.value)}
+        />
       </EmailBox>
       <NameBox valid={true}>
         <NameText>이름</NameText>
-        <NameInput />
+        <NameInput
+          value={name}
+          onChange={(e) => nameOnChange(e.target.value)}
+        />
       </NameBox>
-      <PwBox valid={true}>
+      <PwBox valid={pw.length > 0 ? pwPattern.test(pw) : true}>
         <PwText>비밀번호</PwText>
-        <PwInput />
+        <PwInput
+          type="password"
+          value={pw}
+          onChange={(e) => pwOnChange(e.target.value)}
+        />
       </PwBox>
-      <PhoneBox valid={true}>
-        <PhoneText>전화번호</PhoneText>
-        <PhoneInput />
-      </PhoneBox>
-      <SaveBtn>저장</SaveBtn>
+      <SaveBtn onClick={() => saveBtnOnClick()}>저장</SaveBtn>
     </SignUpContainer>
   );
 };
@@ -54,7 +107,7 @@ const IdBox = styled.div<{ valid: boolean }>`
           border-color: #ff3a3a;
         }
         &::after {
-          content: "영어,숫자를 포함 8~20자 이내로 입력해주세요.";
+          content: "영어,숫자를 포함해서 4~15자 이내로 입력해주세요.";
           position: absolute;
           top: 100%;
           left: 20%;
@@ -80,7 +133,17 @@ const IdInput = styled.input`
   font-size: ${({ theme }) => theme.fontSizes.small};
 `;
 
-const EmailBox = styled(IdBox)``;
+const EmailBox = styled(IdBox)`
+  ${(props) => {
+    if (!props.valid) {
+      return css`
+        &::after {
+          content: "이메일 형식에 맞게 입력해주세요.";
+        }
+      `;
+    }
+  }}
+`;
 const EmailText = styled(IdText)``;
 const EmailInput = styled(IdInput)``;
 
@@ -88,13 +151,19 @@ const NameBox = styled(IdBox)``;
 const NameText = styled(IdText)``;
 const NameInput = styled(IdInput)``;
 
-const PwBox = styled(IdBox)``;
+const PwBox = styled(IdBox)`
+  ${(props) => {
+    if (!props.valid) {
+      return css`
+        &::after {
+          content: "영어,숫자를 포함 8~20자 이내로 입력해주세요.";
+        }
+      `;
+    }
+  }}
+`;
 const PwText = styled(IdText)``;
 const PwInput = styled(IdInput)``;
-
-const PhoneBox = styled(IdBox)``;
-const PhoneText = styled(IdText)``;
-const PhoneInput = styled(IdInput)``;
 
 const SaveBtn = styled.button`
   margin-top: 20px;
