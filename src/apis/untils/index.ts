3cfbcab3ffad1d4367edef4191366e0066227b4d
taewok <taewok5163@naver.com>
2023-06-17 17:45:06 +0900

    #47 feat: api 통신중 "토큰이 유효하지 않습니다" error일 경우 통신을 가로채서 removeCookie 함수 실행

--- src/apis/untils/index.ts
+++ src/apis/untils/index.ts
@@ -1,5 +1,19 @@
 import axios from "axios";
+import { removeCookie } from "../../components/cookie/cookie";
 
 export const api = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
 });
+
+// 응답 인터셉터 추가하기
+api.interceptors.response.use(
+  function (response) {
+    // 응답 데이터가 있는 작업 수행
+    return response;
+  },
+  function (error) {
+    // 응답 오류가 있는 작업 수행
+    if (error.response.data.message === "토큰이 유효하지 않습니다")
+      removeCookie();
+  }
+);
