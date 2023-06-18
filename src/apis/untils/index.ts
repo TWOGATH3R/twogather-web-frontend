9e99f6b0c7a3b04a945c2bbed2b96c7d69b43037
taewok <taewok5163@naver.com>
2023-06-18 15:24:29 +0900

    #47 feat: error code로 비교로 변경

--- src/apis/untils/index.ts
+++ src/apis/untils/index.ts
@@ -12,9 +12,11 @@ api.interceptors.response.use(
     return response;
   },
   function (error) {
-    console.log(error)
+    const {
+      response: { status },
+    } = error;
     // 응답 오류가 있는 작업 수행
-    if (error.response.data.message === "토큰이 유효하지 않습니다") {
+    if (status === 401) {
       removeCookie();
       window.location.replace("/login");
     }
