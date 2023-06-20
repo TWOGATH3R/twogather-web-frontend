a034811c4f12d6dd22661cfbc05531a1474d35dd
taewok <taewok5163@naver.com>
2023-06-20 12:35:58 +0900

    refactor: 가입에서 사업자 정보 check를 할 필요가 없어져 사업자 인증 api 삭제

--- src/apis/queries/SignUpQuery.ts
+++ src/apis/queries/SignUpQuery.ts
@@ -1,33 +1,11 @@
-import axios from "axios";
 import { api } from "../untils";
-
-//사업자 정보가 일치하는지 확인
-export const buisnessCheckMutaionPostInfo = async (
-  number: string,
-  name: string,
-  date: string
-) => {
-  const serviceKey = process.env.REACT_APP_SERVICEKEY;
-  const res = await axios.post(
-    `http://api.odcloud.kr/api/nts-businessman/v1/validate?serviceKey=${serviceKey}`,
-    {
-      businesses: [
-        {
-          b_no: number,
-          start_dt: date,
-          p_nm: name,
-        },
-      ],
-    }
-  );
-  return res.data;
-};
 
 //이메일 인증번호 보내기
 export const emailCheckMutaionPostEmail = async (email: string) => {
   const res = await api.post(`/api/email`, {
     email: email,
   });
+  console.log(res)
   return res.data;
 };
 
