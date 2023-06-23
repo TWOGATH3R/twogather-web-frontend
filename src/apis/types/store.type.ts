b160a6cdd4fbca8129e7021ddba9c21a82a5ac4a
taewok <taewok5163@naver.com>
2023-06-23 14:35:13 +0900

    #4 feat: 가게등록시 메뉴리스트 등록 api test 완료

---
+++ src/apis/types/store.type.ts
@@ -0,0 +1,44 @@
+//가게등록 api props type정의
+export type postEnrollShopInfoProps = {
+  storeName: string;
+  address: string;
+  phone: string;
+  businessNumber: string;
+  businessName: string;
+  businessStartDate: string;
+  keywordIdList: number[];
+  categoryId: number;
+};
+
+//가게등록 api response type정의
+export type postEnrollShopInfoResponse = {
+  data: {
+    storeId: number;
+    storeName: string;
+    address: string;
+    phone: string;
+    businessNumber: string;
+    businessName: string;
+    businessStartDate: string;
+    keywordIdList: string[];
+    categoryId: number;
+  };
+};
+
+//메뉴등록 api props type정의
+export type postMenuListProps = {
+  id: number;
+  shopMenuName: string;
+  shopMenuPrice: string;
+};
+
+//메뉴등록 api response type정의
+export type postMenuListResponse = {
+  data: [
+    {
+      menuId: number;
+      name: string;
+      price: number;
+    }
+  ];
+};
