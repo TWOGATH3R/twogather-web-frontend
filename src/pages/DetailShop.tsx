fb0e6e93283aa704c99a4138d5ae834a59c19672
taewok <taewok5163@naver.com>
2023-06-25 15:23:45 +0900

    #74 feat: query string으로 storeId 추출 후 recoil로 저장

--- src/pages/DetailShop.tsx
+++ src/pages/DetailShop.tsx
@@ -1,10 +1,58 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import ShopImgInfo from "../components/detailShop/ShopImgInfo";
 import ReviewEnroll from "../components/detailShop/ReviewEnroll";
 import Reviews from "../components/detailShop/Reviews";
+import { useMutation } from "@tanstack/react-query";
+import { getStoreOne } from "../apis/queries/storeQuery";
+import { useRecoilState } from "recoil";
+import {
+  Address,
+  CategoryName,
+  KeywordList,
+  LikeCount,
+  Phone,
+  StoreId,
+  StoreName,
+} from "../store/storeDetailAtom";
+import { useSearchParams } from "react-router-dom";
 
 export default function DetailShop() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const [storeId, setStoreId] = useRecoilState(StoreId);
+  const [address, setAddress] = useRecoilState(Address);
+  const [keywordList, setKeywordList] = useRecoilState(KeywordList);
+  const [likeCount, setLikeCount] = useRecoilState(LikeCount);
+  const [phone, setPhone] = useRecoilState(Phone);
+  const [storeName, setStoreName] = useRecoilState(StoreName);
+  const [category, setCategory] = useRecoilState(CategoryName);
+
+  useEffect(()=>{
+    setStoreId(Number(searchParams.get("storeId")));
+  },[])
+
+  const { mutate: getStoreInfo } = useMutation(() => getStoreOne(storeId), {
+    onSuccess: (res) => {
+      const data = res.data;
+      setAddress(data.address);
+      setCategory(data.categoryName);
+      setKeywordList(data.keywordList);
+      setLikeCount(data.likeCount);
+      setPhone(data.phone);
+      setStoreId(data.storeId);
+      setStoreName(data.storeName);
+    },
+    onError: (err) => {
+      console.log(err)
+      alert("존재하지 않거나 허용되지 않은 가게입니다.");
+    },
+  });
+
+  useEffect(() => {
+    getStoreInfo();
+  }, [getStoreInfo]);
+
   return (
     <DetailShopContainer>
       <ShopImgInfo />
