353879def29db6b8d9dfd858b688107b94313379
taewok <taewok5163@naver.com>
2023-06-28 20:14:32 +0900

    #74 feat: 리뷰 총갯수 ui 표현

--- src/components/detailShop/ShopInfo.tsx
+++ src/components/detailShop/ShopInfo.tsx
@@ -10,6 +10,7 @@ import {
   Phone,
   StoreId,
   StoreName,
+  TotalReviewCount,
 } from "../../store/storeDetailAtom";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import {
@@ -31,6 +32,8 @@ const ShopInfo = () => {
   const likeCount = useRecoilValue(LikeCount);
   const phone = useRecoilValue(Phone);
   const storeName = useRecoilValue(StoreName);
+
+  const totalReviewCount = useRecoilValue(TotalReviewCount);
 
   const [checkDay, setCheckDay] = useState({ ko: "월", en: "MONDAY" });
 
@@ -70,18 +73,12 @@ const ShopInfo = () => {
       console.log(err.response.data.message);
     },
   });
-  //DB에 저장된 검색가능한 키워드 리스트 가져오기
-  const { data: likeList } = useQuery(
-    ["myLikeList"],
-    () => getMyLikeList(memberId),
-    {
-      onSuccess: (res) => {
-        setLikeBoolean(
-          res.data.map((value) => value.storeId).includes(storeId)
-        );
-      },
-    }
-  );
+  //고객이 좋아요 누른 가게 리스트 가져오기
+  const { mutate: getLikeList } = useMutation(() => getMyLikeList(memberId), {
+    onSuccess: (res) => {
+      setLikeBoolean(res.data.map((value) => value.storeId).includes(storeId));
+    },
+  });
   //좋아요 누르기
   const { mutate: saveLike } = useMutation(() => postLike(storeId, memberId), {
     onError: (err: any) => {
@@ -101,6 +98,7 @@ const ShopInfo = () => {
   useEffect(() => {
     getOpenHourList();
     getMenu();
+    if (getCookie("accessToken") !== undefined) getLikeList();
   }, []);
 
   const dayList = [
@@ -131,8 +129,8 @@ const ShopInfo = () => {
         setLikeBoolean(!likeBoolean);
         if (!likeBoolean) saveLike();
         else likeDelete();
-      } else alert("로그인 후 이용해주세요");
-    } else alert("일반 고객만 좋아요가 가능합니다");
+      } else alert("일반 고객 로그인 후 좋아요가 가능합니다");
+    } else alert("일반 고객 로그인 후 좋아요가 가능합니다");
   };
 
   return (
@@ -163,7 +161,7 @@ const ShopInfo = () => {
         <ReviewHeartCountBox>
           <ReviewCountBox>
             <ReviewCount />
-            <span>54</span>
+            <span>{totalReviewCount}</span>
           </ReviewCountBox>
           <HeartCountBox>
             <HeartCount />
@@ -254,11 +252,11 @@ const ReviewHeartCountBox = styled.div`
 `;
 const ReviewCountBox = styled.div`
   display: flex;
-  justify-content: space-between;
   margin-right: 20px;
   min-width: 50px;
   span {
     margin-top: -3px;
+    margin-left: 3px;
     color: #a5a5a5;
   }
 `;
