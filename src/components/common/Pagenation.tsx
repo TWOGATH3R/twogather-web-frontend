ed4f1f31c41d30e9e9b1f7ddb81712b96bb91601
taewok <taewok5163@naver.com>
2023-06-28 20:24:06 +0900

    #74 feat: 페이지네이션 재활용 컴포넌트 생성

---
+++ src/components/common/Pagenation.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Pagination from "react-js-pagination";
+import styled from "styled-components";
+
+interface infoType {
+  page: any;
+  pageOnChange: any;
+  totalCount: any;
+}
+const Pagenation = ({ page, pageOnChange, totalCount }: infoType) => {
+  return (
+    <PaginationBox>
+      <Pagination
+        activePage={page}
+        itemsCountPerPage={5}
+        totalItemsCount={totalCount}
+        pageRangeDisplayed={5}
+        prevPageText="‹"
+        nextPageText="›"
+        onChange={(page) => pageOnChange(page)}
+      />
+    </PaginationBox>
+  );
+};
+
+const PaginationBox = styled.div`
+  a {
+    color: black;
+  }
+  ul {
+    display: flex;
+    justify-content: center;
+    list-style: none;
+    li {
+      padding: 3px 10px;
+    }
+  }
+  ul.pagination li.active a {
+    color: red;
+  }
+`;
+
+export default Pagenation;
